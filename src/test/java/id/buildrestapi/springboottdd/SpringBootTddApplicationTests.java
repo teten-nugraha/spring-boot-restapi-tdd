8fd7c178325cdb7281f5793a5f0fe369c972f6a0
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-10-01 17:03:51 +0700

    first steup

---
+++ src/test/java/id/buildrestapi/springboottdd/SpringBootTddApplicationTests.java
@@ -0,0 +1,13 @@
+package id.buildrestapi.springboottdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
